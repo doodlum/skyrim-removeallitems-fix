2c188edc99a24c0e32650958b127e0c08052d60d
Tim <15017472+doodlum@users.noreply.github.com>
2023-05-06 17:39:07 +0100

    Finished

--- src/RemoveAllItemsFix.cpp
+++ src/RemoveAllItemsFix.cpp
@@ -1,45 +1,39 @@
 #include "RemoveAllItemsFix.h"
 #include <detours/Detours.h>
 
-decltype(&RemoveAllItemsFix::hk_Papyrus_RemoveAllItems) ptr_Papyrus_RemoveAllItems;
-
-typedef RE::BGSEncounterZone* (*GetEncounterZone_t)(RE::TESObjectREFR* This);
-
-struct Papyrus_RemoveAllItems_ExtraDataList_GetInventoryChanges
+// Note: Does not transfer the patched items to containers
+struct GameFunc_RemoveAllItems_GetReferences
 {
-	static RE::InventoryChanges* thunk(RE::ExtraDataList* a_extraDataList)
+	static bool thunk(uintptr_t a1, uintptr_t a2, uintptr_t a3, RE::TESObjectREFR* ref, uint32_t a5, uintptr_t a6, uintptr_t a7, uintptr_t a8, uintptr_t a9)
 	{
-		RE::TESObjectREFR* actor = SKSE::stl::adjust_pointer<RE::Actor>(a_extraDataList, -112);
+		auto ret = func(a1, a2, a3, ref, a5, a6, a7, a8, a9);
 
-		if (auto scriptFactory = RE::IFormFactory::GetConcreteFormFactoryByType<RE::Script>()) {
-			if (auto script = scriptFactory->Create()) {
-				script->SetCommand("removeitem 0000000F");
-				script->CompileAndRun(actor);
-				script->SetCommand("removeitem 0001397D");
-				script->CompileAndRun(actor);
-				delete script;
+		if (ref == RE::PlayerCharacter::GetSingleton()) {
+			if (auto scriptFactory = RE::IFormFactory::GetConcreteFormFactoryByType<RE::Script>()) {
+				if (auto script = scriptFactory->Create()) {
+					auto inv = ref->GetInventory([&](const RE::TESBoundObject& a_object) {
+						return a_object.GetFormID() == 0xF || a_object.GetFormID() == 0x1397D;
+					});
+
+					if (!inv.empty()) {
+						for (const auto& [item, data] : inv) {
+							script->SetCommand(std::format("removeitem {:X} {}", item->GetFormID(), data.first));
+							logger::debug("removeitem {:X} {}", item->GetFormID(), data.first);
+							script->CompileAndRun(ref);
+						}
+					}
+
+					delete script;
+				}
 			}
 		}
 
-		return func(a_extraDataList);
+		return ret;
 	}
 	static inline REL::Relocation<decltype(thunk)> func;
 };
 
-static void Install()
-{
-	stl::write_thunk_call<Papyrus_RemoveAllItems_ExtraDataList_GetInventoryChanges>(REL::RelocationID(55684, 56215).address() + REL::Relocate(0x19, 0x19));
-
-	logger::info("Installed RemoveAllItems hook");
-}
-
-
-void RemoveAllItemsFix::InstallHooks()
-{
-	logger::info("Installing Papyrus::RemoveAllItems function hook\n\n");
-	*(uintptr_t*)&ptr_Papyrus_RemoveAllItems = Detours::X64::DetourFunction(REL::RelocationID(55684, 56215).address(), (uintptr_t)&hk_Papyrus_RemoveAllItems);
-	Install();
-}
+decltype(&RemoveAllItemsFix::hk_Papyrus_RemoveAllItems) ptr_Papyrus_RemoveAllItems;
 
 void RemoveAllItemsFix::hk_Papyrus_RemoveAllItems(RE::BSScript::Internal::VirtualMachine* a_vm, RE::VMStackID a_stackID, RE::TESObjectREFR* a_ref, RE::TESObjectREFR* a_target, bool a_keepOwnership, bool a_removeQuestItems)
 {
@@ -50,10 +44,20 @@ void RemoveAllItemsFix::hk_Papyrus_RemoveAllItems(RE::BSScript::Internal::Virtua
 
 		if (!inv.empty()) {
 			for (const auto& [item, data] : inv) {
-				remove_item(a_ref, item, data.first, true, a_target, a_stackID, a_vm);
+				RemoveItem(a_ref, item, data.first, true, a_target, a_stackID, a_vm);
 			}
 		}
 	}
 
 	(ptr_Papyrus_RemoveAllItems)(a_vm, a_stackID, a_ref, a_target, a_keepOwnership, a_removeQuestItems);
 }
+
+void RemoveAllItemsFix::InstallHooks()
+{
+	logger::info("Installing GameFunc::RemoveAllItems hook");
+	stl::write_thunk_call<GameFunc_RemoveAllItems_GetReferences>(REL::RelocationID(21523, 22005).address() + 0x58);
+
+	logger::info("Installing Papyrus::RemoveAllItems function hook\n\n");
+	*(uintptr_t*)&ptr_Papyrus_RemoveAllItems = Detours::X64::DetourFunction(REL::RelocationID(55684, 56215).address(), (uintptr_t)&hk_Papyrus_RemoveAllItems);
+}
+
--- src/RemoveAllItemsFix.h
+++ src/RemoveAllItemsFix.h
@@ -12,117 +12,13 @@ public:
 
 	static void InstallHooks();
 
-	static void remove_item(RE::TESObjectREFR* a_ref, RE::TESBoundObject* a_item, std::uint32_t a_count, bool a_silent, RE::TESObjectREFR* a_otherContainer, RE::VMStackID a_stackID, RE::BSScript::Internal::VirtualMachine* a_vm)
+	// https://github.com/powerof3/PapyrusExtenderSSE/blob/master/src/Papyrus/Util/Inventory.cpp
+	static void RemoveItem(RE::TESObjectREFR* a_ref, RE::TESBoundObject* a_item, std::uint32_t a_count, bool a_silent, RE::TESObjectREFR* a_otherContainer, RE::VMStackID a_stackID, RE::BSScript::Internal::VirtualMachine* a_vm)
 	{
-		using func_t = decltype(&remove_item);
+		using func_t = decltype(&RemoveItem);
 		REL::Relocation<func_t> func{ REL::RelocationID(56261, 56647) };
 		return func(a_ref, a_item, a_count, a_silent, a_otherContainer, a_stackID, a_vm);
 	}
 
 	static void hk_Papyrus_RemoveAllItems(RE::BSScript::Internal::VirtualMachine* a_vm, RE::VMStackID a_stackID, RE::TESObjectREFR* a_ref, RE::TESObjectREFR* a_target, bool a_keepOwnership, bool a_removeQuestItems);
-
-protected:
-	//struct Hooks
-	//{
-	//	struct GameFunc_RemoveAllItems_ExtraDataList_GetInventoryChanges
-	//	{
-	//		static RE::InventoryChanges* thunk(RE::InventoryChanges* changes, RE::TESObjectREFR* source, RE::TESObjectREFR* target, int64_t unk1, bool abKeepOwnership, bool abRemoveQuestItems)
-	//		{
-	//			RE::TESObjectREFR* actor = SKSE::stl::adjust_pointer<RE::Actor>(a_extraDataList, -112);
-
-	//			if (auto scriptFactory = RE::IFormFactory::GetConcreteFormFactoryByType<RE::Script>()) {
-	//				if (auto script = scriptFactory->Create()) {
-	//					script->SetCommand("removeitem 0000000F");
-	//					script->CompileAndRun(actor);
-	//					script->SetCommand("removeitem 0001397D");
-	//					script->CompileAndRun(actor);
-	//					delete script;
-	//				}
-	//			}
-
-	//			return func(a_extraDataList);
-	//		}
-	//		static inline REL::Relocation<decltype(thunk)> func;
-	//	};
-
-	//	struct Papyrus_RemoveAllItems_ExtraDataList_GetInventoryChanges
-	//	{
-	//		static RE::InventoryChanges* thunk(RE::ExtraDataList* a_extraDataList)
-	//		{
-	//			RE::TESObjectREFR* actor = SKSE::stl::adjust_pointer<RE::Actor>(a_extraDataList, -112);
-
-	//			if (auto scriptFactory = RE::IFormFactory::GetConcreteFormFactoryByType<RE::Script>()) {
-	//				if (auto script = scriptFactory->Create()) {
-	//					script->SetCommand("removeitem 0000000F");
-	//					script->CompileAndRun(actor);
-	//					script->SetCommand("removeitem 0001397D");
-	//					script->CompileAndRun(actor);
-	//					delete script;
-	//				}
-	//			}
-
-	//			return func(a_extraDataList);
-	//		}
-	//		static inline REL::Relocation<decltype(thunk)> func;
-	//	};
-
-	//	static void Install()
-	//	{
-	//		stl::write_thunk_call<GameFunc_RemoveAllItems_ExtraDataList_GetInventoryChanges>(REL::RelocationID(21523, 22005).address() + REL::Relocate(0xBA, 0xBA));
-	//		stl::write_thunk_call<Papyrus_RemoveAllItems_ExtraDataList_GetInventoryChanges>(REL::RelocationID(55684, 56215).address() + REL::Relocate(0x46, 0x46));
-
-	//		logger::info("Installed RemoveAllItems hooks");
-	//	}
-	/*struct Hooks
-	{
-		struct GameFunc_RemoveAllItems_ExtraDataList_GetInventoryChanges
-		{
-			static RE::InventoryChanges* thunk(RE::ExtraDataList* a_extraDataList)
-			{
-				RE::TESObjectREFR* actor = SKSE::stl::adjust_pointer<RE::Actor>(a_extraDataList, -112);
-
-				if (auto scriptFactory = RE::IFormFactory::GetConcreteFormFactoryByType<RE::Script>()) {
-					if (auto script = scriptFactory->Create()) {
-						script->SetCommand("removeitem 0000000F");
-						script->CompileAndRun(actor);
-						script->SetCommand("removeitem 0001397D");
-						script->CompileAndRun(actor);
-						delete script;
-					}
-				}
-
-				return func(a_extraDataList);
-			}
-			static inline REL::Relocation<decltype(thunk)> func;
-		};
-
-		struct Papyrus_RemoveAllItems_ExtraDataList_GetInventoryChanges
-		{
-			static RE::InventoryChanges* thunk(RE::ExtraDataList* a_extraDataList)
-			{
-				RE::TESObjectREFR* actor = SKSE::stl::adjust_pointer<RE::Actor>(a_extraDataList, -112);
-
-				if (auto scriptFactory = RE::IFormFactory::GetConcreteFormFactoryByType<RE::Script>()) {
-					if (auto script = scriptFactory->Create()) {
-						script->SetCommand("removeitem 0000000F");
-						script->CompileAndRun(actor);
-						script->SetCommand("removeitem 0001397D");
-						script->CompileAndRun(actor);
-						delete script;
-					}
-				}
-
-				return func(a_extraDataList);
-			}
-			static inline REL::Relocation<decltype(thunk)> func;
-		};
-
-		static void Install()
-		{
-			stl::write_thunk_call<GameFunc_RemoveAllItems_ExtraDataList_GetInventoryChanges>(REL::RelocationID(21523, 22005).address() + REL::Relocate(0x8C, 0x8C));
-			stl::write_thunk_call<Papyrus_RemoveAllItems_ExtraDataList_GetInventoryChanges>(REL::RelocationID(55684, 56215).address() + REL::Relocate(0x19, 0x19));
-
-			logger::info("Installed RemoveAllItems hooks");
-		}*/
-	//};
 };
